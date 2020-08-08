925045c1f3c348e2896ccb61e85f010fe150b514
moh-java4 <moh.java4@gmail.com>
2020-08-08 21:57:26 +0430

    adding font to toolbar

--- app/src/main/java/com/example/la/MainActivity.java
+++ app/src/main/java/com/example/la/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.la;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
