413e431b931f00187db9c4dc8b63aebd83b2b90a
moh-java4 <moh.java4@gmail.com>
2020-08-08 22:07:20 +0430

    adding TypeFace To Activity2

--- app/src/main/java/com/example/la/Activity2.java
+++ app/src/main/java/com/example/la/Activity2.java
@@ -1,14 +1,38 @@
 package com.example.la;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+    private TextView textView1;
+    private boolean changeFont = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        textView1 = findViewById(R.id.text_view1);
+
+        final Typeface t1 = ResourcesCompat.getFont(this, R.font.simonetta_black);
+        final Typeface t2 = ResourcesCompat.getFont(this, R.font.simonetta_regular);
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (changeFont) {
+                    textView1.setTypeface(t1);
+                    changeFont = false;
+                } else {
+                    textView1.setTypeface(t2);
+                    changeFont = true;
+                }
+            }
+        });
     }
 }
